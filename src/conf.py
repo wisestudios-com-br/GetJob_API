d8c036628066d0f75fea2c7165fc86d583dcd6e1
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-07-09 13:53:00 -0300

    Implementada conexões com o BD;

--- src/conf.py
+++ src/conf.py
@@ -1,4 +1,4 @@
 from	pymongo	import MongoClient
 
-db_auth	= MongoClient()
-db_data	= MongoClient()
+db_auth	= MongoClient("mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net").GetJob_Auth
+db_data	= MongoClient("mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net").GetJob_Data
