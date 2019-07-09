c7343e66f0ea700757ca107d370cf4335cc08023
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-07-09 20:24:21 -0300

    Corrigido erro de sintaxe;

--- src/conf.py
+++ src/conf.py
@@ -1,4 +1,4 @@
 import	mongoengine
 
-db_auth	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Auth", alias="auth")
-db_data	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Data", alias="default")
+db_auth	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Auth", alias="auth").GetJob_Auth
+db_data	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Data", alias="default").GetJob_Data
