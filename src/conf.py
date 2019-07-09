ac56cb4e386bbabeb3caad05e0ae4bb66027d13d
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-07-09 19:51:40 -0300

    Corrigido erro de sintaxe;

--- src/conf.py
+++ src/conf.py
@@ -1,4 +1,4 @@
-from	pymongo	import MongoClient
+import	mongoengine
 
-db_auth	= MongoClient("mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net").GetJob_Auth
-db_data	= MongoClient("mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net").GetJob_Data
+db_auth	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Auth", alias="auth")
+db_data	= mongoengine.connect(host="mongodb+srv://api:3y3ccSLZtZQlGdoV@getjob-nagfm.mongodb.net/GetJob_Data", alias="default")
